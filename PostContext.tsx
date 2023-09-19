2f1019a6f2a7f5d42e34b32283624467f694f573
unknown <Thur0Arthur>
2023-09-19 17:29:52 -0300

    primeiro commit

---
+++ PostContext.tsx
@@ -0,0 +1,19 @@
+import { PostActions, postReducer } from "@/reduces/postRecucer";
+import { Post } from "@/types/Post";
+import { Dispatch, ReactNode, createContext, useReducer } from "react";
+
+type PostContextType = {
+    posts: Post[];
+    dispatch: Dispatch<PostActions>;
+}
+export const PostContext = createContext<PostContextType | null>(null);
+
+export const PostProvider = ({children}: {children: ReactNode} ) => {
+    const [posts, dispatch] = useReducer(postReducer, [])
+    
+ 
+
+    return(
+        <PostContext.Provider value={{posts, dispatch}}>{children}</PostContext.Provider>
+    );
+}
